dae18840db0fe15b830693cc7d0399c4f950cfe0
avinash <backon153@gmail.com>
2026-04-20 19:39:46 +0545

    EDA analysis

--- EDA.py
+++ EDA.py
@@ -5,25 +5,25 @@ df = pd.read_csv('Titanic-Dataset.csv')
 from ydata_profiling import ProfileReport
 prof = ProfileReport(df)
 prof.to_file(output_file='output.html')
-# print(df.head())
+print(df.head())
 
-#to plot a number of count in bar graph
-# sns.countplot(x='Sex', data=df)
+# to plot a number of count in bar graph
+sns.countplot(x='Sex', data=df)
 
-#to plot a number of count in pie chart
-# df['Survived'].value_counts().plot(kind='pie',autopct='%1.1f%%')
-# plt.show()
+# to plot a number of count in pie chart
+df['Survived'].value_counts().plot(kind='pie',autopct='%1.1f%%')
+plt.show()
 
-#hist graph plot
-# plt.hist(df['Age'],bins=10)
+# hist graph plot
+plt.hist(df['Age'],bins=10)
 
-#distplot
-# sns.distplot(df['Age'])
-# plt.show()
+# distplot
+sns.distplot(df['Age'])
+plt.show()
 
-#box plot
-# plt.boxplot(df['Fare'])
-# plt.show()
+# box plot
+plt.boxplot(df['Fare'])
+plt.show()
 
-# print(df.describe())
-#skew(normal symmetricity)
+print(df.describe())
+# skew(normal symmetricity)
